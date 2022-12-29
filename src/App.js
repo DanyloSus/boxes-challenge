a4416faba16604ff897cb2954f135da593c722dc
DanyloSus <danylo.sushko19@gmail.com>
2022-12-29 18:53:59 +0200

    Add state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react"
+import boxes from "./boxes"
 
 export default function App() {
-  return(
-    <h1>Check</h1>
-  )
+    const [boxesArr, setBoxes] = React.useState(boxes)
+
+    console.log(boxesArr)
+
+    const box = boxesArr.map(item => (
+      <p key={item.id}>{item.id}</p>
+    ))
+
+    return (
+        <main>
+            <h1>Boxes will go here</h1>
+            {box}
+        </main>
+    )
 }
